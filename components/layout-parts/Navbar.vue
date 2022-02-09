aef0002743ffec0170aa20f1322e3c5dff57b687
bobbykim89 <bobby.sihun.kim@gmail.com>
2022-02-09 00:36:11 -0700

    make initial commit

---
+++ components/layout-parts/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <b-navbar toggleable="md" type="dark" variant="dark">
+    <b-navbar-brand href="/">Stock Tracker</b-navbar-brand>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav class="ml-md-5">
+        <b-nav-item to="/" class="mx-auto mx-md-0"> Link </b-nav-item>
+        <b-nav-item to="/dummy" class="mx-auto mx-md-0"> toDummy </b-nav-item>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item-dropdown text="Lang">
+          <b-dropdown-item href="#">EN</b-dropdown-item>
+          <b-dropdown-item href="#">ES</b-dropdown-item>
+          <b-dropdown-item href="#">RU</b-dropdown-item>
+          <b-dropdown-item href="#">FA</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        <b-nav-item-dropdown right>
+          <!-- Using 'button-content' slot -->
+          <template #button-content>
+            <em>User</em>
+          </template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Navbar />
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/layout-parts/Navbar.vue'
+export default {
+  components: {
+    Navbar,
+  },
+}
+</script>
+
+<style></style>
